9d86b6b86d3ca6715c97f99dd4cba288da672567
PEDRO  PRATES <157413476+PedroPrates08@users.noreply.github.com>
2024-01-22 16:20:58 -0300

    Add files via upload

---
+++ aprovado.py
@@ -0,0 +1,12 @@
+#Aprovador de idade 
+
+#escreva sua idade 
+idade = int(input("Escreva sua idade: "))
+
+#condições para a idade escrita
+if idade >= 18:
+    print("idade aprovada")
+
+elif idade <= 17: 
+    print("idade não aprovada")
+
